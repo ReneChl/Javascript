c5f120b1135f6f87e7f70c587d53baadfaf5ada2
ReneChl <rene258chl@gmail.com>
2025-05-13 21:47:05 -0300

    aula 08
    13/05/25

--- aula07.js
+++ aula07.js
@@ -19,11 +19,11 @@ console.log(resultadoMultiplicacao);
 
 // Podemos utiliza-los em outras situacões, como incrementar uma frase por exemplo (comojá fizemos em sala).
 
-const texto1 = 'Olá, meu nome é José';
-const texto2 = 'Eu tenho 56 anos de idade';
+const texto1 = 'Olá, meu nome é José ';
+const texto2 = 'Eu tenho 56 anos de idade ';
 
 const textoFinal = texto1 + texto2;
-//console.log(textoFinal);
+console.log(textoFinal);
 
 
 let x = 5;
---
+++ aula08.js
@@ -0,0 +1,59 @@
+// Operadores Comparação: São usadas para comparar os valorese e retornam um valor vooleano (true ou false)
+
+// usando ==
+let x = 10;
+let y = 50;
+
+console.log(x == y);
+
+
+let x = 10;
+let y = 10;
+
+console.log(x == y);
+
+let x = 10;
+let y = 'texto';
+
+console.log(x === y);
+
+// usando !=
+let x = 10;
+let y = 10;
+
+console.log(x !== y); // 
+
+// usando !=
+let x = 10;
+let y = 'teste';
+
+console.log(x !== y); 
+
+//SETAMOS TESTANDO SE O VALOR DE "X" É IGUAL AO VALOR DE "Y"
+//AS RESPOSTAS SERÃO TRUE(para verdadeira/afirmação) false (para falso/negação)
+
+let frutas = "Banana";
+let frutas2 = "maça";
+// Aqui estamos perguntado se o valorda variá vel "frutas" é DIFERENTE da variável "frutas2
+console.log(frutas !== frutas2);
+
+let numero = 20;
+let segundoNumero = 258;
+
+console;log(bunero > 30); // (>) MAIOR QUE
+
+let numero = 20;
+let segundoNumero = 258;
+// Estamos utilizando (maior que >) e (menor que <) para comparar determinadas informaç
+console;log(numeunero < 30); // (>) MENOR QUE
+
+
+let numero = 20;
+let segundoNumero = 258;
+
+console;log(numero >=19)
+
+let numero = 50;
+let segundoNumero = 258;
+
+console;log(numero >=19) //verdadero 
