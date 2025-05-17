6384a62d63caddcf471a7aa5476d279367bc2beb
ReneChl <rene258chl@gmail.com>
2025-05-16 21:19:38 -0300

    jascript
    16/05/25

--- aula09.js
+++ aula09.js
@@ -19,21 +19,21 @@ const Carta = {
 //console.log(`Meu personagem se chama: ${Carta.nome}`);
 
 
-const Carta = {
+const Carta1 = {
     nome: "Pokemon",
     habilidade: 95,
     defesa: 40,
     ataque:89,
     slogan:"Pokemon Amarelo rs"
 };
-console.log(`Meu personagem se chama: ${Carta.nome}, tem habilidade ${Carta.habilidade}, com defensa:${Carta.defensa}, e ataque:${Carta.ataque}`);
+console.log(`Meu personagem se chama: ${Carta.nome}, tem habilidade ${Carta.habilidade}, com defensa:${Carta.defesa}, e ataque ${Carta.ataque}`);
 
 
-const Carta = {
-    nome:,
-    habilidade: 95,
-    defesa: 40,
-    ataque:89,
-    slogan:"Pokemon Amarelo rs"
+const Carta2 = {
+    nome:"Modas LR",
+    experiencia: 6,
+    funcionarios: 4,
+    contato:951178439,
+    slogan:"Modas LR rs"
 };
-console.log(`Meu personagem tem habilidade: ${Carta.habilidade}`);
+console.log(`: ${Carta.habilidade}`);
