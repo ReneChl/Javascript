90c61959a8a720ffd975269e034aa941f939d27f
ReneChl <rene258chl@gmail.com>
2025-05-29 21:20:35 -0300

    javascript
    29/05/025

--- aula13/script.js
+++ aula13/script.js
@@ -1,14 +1,24 @@
 //aqui estamos recuperando nosso ID que esta no HTML
 
+// estamos criando funciones
+
 let meuElemento = document.getElementById("meuElemento");
 
 
 function tornarCirculo() {
-    meuElemento.classList.add("circle");//adiciono uma class para arredondar o quadrado
-    meuElemento.classList.remove("square");// removo uma class para arredondar a caixa
+    meuElemento.classList.add("circle");//adiciono uma class para arredondar o quadrado, os puntos es para juntar os elementos (concatenar elementos)
+    meuElemento.classList.remove("square");// removo uma class para arredondar a caixa, puntos es para juntar os elementos (concatenar elementos)
 }
 
 function tornarQuadrado() {
-    meuElemento.classList.add("square")//adiciono uma class para arredondar o quadrado
-    meuElemento.classList.remove("circle")// removo uma class para arredondar a caixa
+    meuElemento.classList.add("square");//adiciono uma class para arredondar o quadrado
+    meuElemento.classList.remove("circle");// removo uma class para arredondar a caixa
 }
+
+function alternarFormato() {
+    meuElemento.classList.toggle("circle");
+    meuElemento.classList.toggle("scuadre");
+
+}
+
+// toggle é responsável por vereficar o elemento atual (fubciona como se)
